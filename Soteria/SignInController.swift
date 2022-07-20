30e3fa91dbf3420066c17b40e82ad8d99154015b
Monique Reed <moniquereed101@gmail.com>
2022-07-20 13:22:34 -0400

    sign in with apple button finished but not working

--- Soteria/SignInController.swift
+++ Soteria/SignInController.swift
@@ -20,10 +20,11 @@ class SignInController: UIViewController {
         // Do any additional setup after loading the view.
         
         view.addSubview(signInButton)
+        signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
     }
     
     
-    
+    //sign in with apple button
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         signInButton.frame = CGRect(x: 0, y: 0, width: 250, height: 50)
@@ -31,7 +32,45 @@ class SignInController: UIViewController {
         
             
         }
+    @objc func didTapSignIn(){
+        let provider = ASAuthorizationAppleIDProvider()
+        let request  = provider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
     }
+    
+    }
+
+extension SignInController: ASAuthorizationControllerDelegate{
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("failed")
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        switch authorization.credential{
+        case let credentials as ASAuthorizationAppleIDCredential:
+            let firstname = credentials.fullName?.givenName
+            let email = credentials.email
+            break
+        default:
+            break
+        }
+    }
+    
+    
+}
+
+extension SignInController: ASAuthorizationControllerPresentationContextProviding{
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
+    }
+}
+
     /*
     // MARK: - Navigation
 
